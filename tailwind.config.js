0dedbea576758a1132933941852706b9c5cd7a95
tatobrito <gnothisway@hotmail.com>
2024-10-22 23:22:15 -0300

    adc cores no projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Corrige duplicação
   theme: {
     extend: {
       fontFamily: {
         graphik: ["Graphik", "sans-serif"],
-        "graphik-bold": ["Graphik-bold", "sans-serif"]
+        "graphik-bold": ["Graphik-bold", "sans-serif"],
       },
-    },
-  },
+      colors: {
+        primary: "#2091F9",
+        text: "#252B42",
+        'secondary-text': "#374754",
+        'dark-background': "#252B42",
+      }, 
+    }, 
+  }, 
   plugins: [],
 };
